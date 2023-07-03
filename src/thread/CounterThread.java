69070ee7cf9381af29fadaca1462578cb514a5e4
XIEXIE143 <xjy143@outlook.com>
2023-07-03 11:07:45 +0900

    add Counter

---
+++ src/thread/CounterThread.java
@@ -0,0 +1,22 @@
+package thread;
+
+public class CounterThread implements Runnable {
+
+    public static Integer count = 5;
+    public long interval = 1000;
+
+    @Override
+    public void run() {
+        while (count > 0) {
+            try {
+                Thread.sleep(interval);
+                String message = Thread.currentThread().getName() + ":" + count;
+                System.out.println(message);
+                count--;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
